5cfaed5ac960ad094c6105051739c2fbf7bc4ed7
GerbenKoopman <gerbennkoopman@gmail.com>
2025-01-11 13:24:53 +0000

    Deploying to gh-pages from @ GerbenKoopman/GerbenKoopman.github.io@2e910c7f9da34946f22232eb039a8b430e000cd8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,13 +462,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/gerben-koopman", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
